13bc7dd476b0916fcb4fe7e1e60713cf83100d6b
Adegbehingbe, Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2023-09-25 19:10:00 +0100

    Setup whitenoise for serving static files

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -18,6 +18,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    # whitenoise
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
