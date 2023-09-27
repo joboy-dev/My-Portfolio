357ae8a8ade8453246d75de820ba4166dd29d96b
Adegbehingbe, Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2023-09-27 19:23:03 +0100

    Changed allowed hosts in settings.py and edited build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -o errexit
 
 python -m pip install --upgrade pip
-
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
+python manage.py makemigrations
+python manage.py makemigrations portfolio
 python manage.py migrate
